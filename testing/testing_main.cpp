3f0b3fa7f61c0918d424ba726ed23dbc40a01b22
tsekh <tsekh@LAPTOP-QKO6T69Q>
2024-09-23 14:46:59 +0300

    chore: подключены гугл тесты + настроена сборка

---
+++ testing/testing_main.cpp
@@ -0,0 +1,19 @@
+#define GTEST_BREAK_ON_FAILURE 1
+#define GTEST_CATCH_EXCEPTIONS 0
+#define GTEST_HAS_SEH 0
+#define _VARIADIC_MAX 10 /* for gtest */
+#include "gtest/gtest.h"
+
+#include <src/vle_solvers.h>
+
+#include <iostream>
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+#ifdef _WIN32
+    std::wcout.imbue(std::locale("rus_rus.866"));
+#endif
+    int res = RUN_ALL_TESTS();
+    return res;
+}
