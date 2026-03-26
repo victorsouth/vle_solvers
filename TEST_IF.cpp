56ac1193fff72b1cb210f3a3bbf4debb3ac1daf1
Егор Коваль < @DESKTOP-13JSTS2>
2026-03-26 10:35:35 +0300

    Добавление AI-rewiew

---
+++ TEST_IF.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int x = 10;
+    
+        if (x > 0) std::cout << "Положительное" << std::endl;
+        else std::cout << "Не положительное" << std::endl;
+    
+    return 0;
+}
