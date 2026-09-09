2ed80a3c8e4d759d79d136382fee34295b1006f9
fedorchenko.yp <yfdrnchk@mail.ru>
2026-09-09 14:29:01 +0300

    fix

--- vle/fluid/solvers/rachford_rice.cpp
+++ vle/fluid/solvers/rachford_rice.cpp
@@ -175,7 +175,7 @@ fixed_solver_result_t<1> rachford_rice2_t::solve(fixed_solver_result_analysis_t<
     // было 1e-10 - магическое число
     // Повышение точности при низких температурах - следствие тестов HasEnoughAccuracyBubblePoint
     // в остальном диапазоне "сарая точность" для сохранения производительности
-    solver_parameters.argument_increment_norm = (temperature<KELVIN_OFFSET/4.)?100.*std::numeric_limits<double>::epsilon():1.e-10;
+    solver_parameters.argument_increment_norm = (temperature<KELVIN_OFFSET/4.)?10.*std::numeric_limits<double>::epsilon():1.e-10;
     solver_parameters.line_search_fail_action = line_search_fail_action_t::TreatAsFail;
     solver_parameters.line_search.iteration_count = 100;
 
